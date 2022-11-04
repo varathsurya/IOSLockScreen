10916b9181f8c115fe8347a27a23d215a43e425c
varath <m.varathsurya@gmail.com>
2022-11-04 22:15:12 +0530

    footer

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import wallpaper from './assets/images/wallpaper.webp'
-import {Ionicons} from "@expo/vector-icons"
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import wallpaper from "./assets/images/wallpaper.webp";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <ImageBackground source={wallpaper} style={styles.container}>
       <View style={styles.header}>
-      <Ionicons name='ios-lock-closed' size={20} color="white" />
-      <Text style={styles.date}>Friday 30 September</Text>
-      <Text style={styles.time}>15:26</Text>
+        <Ionicons name="ios-lock-closed" size={20} color="white" />
+        <Text style={styles.date}>Friday 30 September</Text>
+        <Text style={styles.time}>15:26</Text>
+      </View>
+      {/* Notification List */}
+
+      <View style={styles.footer}>
+        <View style={styles.icon}>
+          <MaterialCommunityIcons name="flashlight" size={24} color="white" />
+        </View>
+        <View style={styles.icon}>
+          <Ionicons name="ios-camera" size={24} color="white" />
+        </View>
       </View>
       <StatusBar style="auto" />
     </ImageBackground>
@@ -19,22 +29,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "stretch",
   },
   header: {
-    alignItems:'center',
-    justifyContent:'center',
-    height: 250
+    alignItems: "center",
+    justifyContent: "center",
+    height: 250,
   },
   date: {
-    fontSize:20,
-    fontWeight: '500',
-    color:'#C3FFFE',
-    marginTop:20
+    fontSize: 20,
+    fontWeight: "500",
+    color: "#C3FFFE",
+    marginTop: 20,
   },
   time: {
-    fontSize:82,
-    fontWeight: 'bold',
-    color:'#C3FFFE'
-  }
+    fontSize: 82,
+    fontWeight: "bold",
+    color: "#C3FFFE",
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: "auto",
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    height: 75,
+  },
+  icon: {
+    backgroundColor: "#00000050",
+    width: 50,
+    aspectRatio: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+  },
 });
